41e0c08eafb828c78cad2cdb13c86c58a50666c0
SirDgor <thedaltonhegg@gmail.com>
2026-03-15 15:35:09 -0500

    Update and rename SQL_JoinQuery to SQL_JoinQuery.sql
    Added description, renamed file to make it a proper .sql

--- SQL_JoinQuery.sql
+++ SQL_JoinQuery.sql
@@ -3,3 +3,4 @@ SELECT p.pin, p.acres_poly, p.use1_desc, s.sale_date, s.sale_value, p.shape
 FROM defaultdb.enterprise.parcels_core p
 LEFT JOIN defaultdb.enterprise.parcel_sales s
 ON p.pin = s.pin;
+-- Joins 2 databases together in 1 query, with data from both databases, along the pin number in both sets.
