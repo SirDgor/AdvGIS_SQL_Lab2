3d84db6cf1e3d8c7e20029de8cb9fe0fe598ef4b
SirDgor <thedaltonhegg@gmail.com>
2026-03-15 15:34:04 -0500

    Update SQL_WhereQuery.sql
    Added Descriptive comment

--- SQL_WhereQuery.sql
+++ SQL_WhereQuery.sql
@@ -2,3 +2,5 @@ CREATE OR REPLACE VIEW student.v_parcels_where AS
 SELECT pin, acres_poly, use1_desc, ttl_mv, year_built, shape
 FROM defaultdb.enterprise.parcels_core
 WHERE acres_poly >= 5;
+
+-- Selects several sets of data from the parcel database, then keeps ones that have an acres_poly greater than or equal to 5.
